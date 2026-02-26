0665d1192776d371a6499b600e5a0a68e23835e0
salima <sahisalima77@gmail.com>
2026-02-26 12:27:48 +0100

    declare route wallet dans server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port=3000
 
 app.use(express.json())
 app.use('/users',userRoutes)
-
+app.use('/wallet',walletRouter)
 
 // hna kanmchiw ngolo express ymkn lih y9ra req lijayn b forme json f etat dyal (POST,PUT)
 app.listen(port,()=>{
